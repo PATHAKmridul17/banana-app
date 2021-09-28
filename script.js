bae6c5f530a039addbfc2acc10d82f66a5c0c4b8
PATHAKmridul17 <90190954+PATHAKmridul17@users.noreply.github.com>
2021-09-28 03:20:22 +0000

    add script

---
+++ script.js
@@ -0,0 +1,29 @@
+
+var btnTranslate= document.querySelector("#btn-translate");
+var txtInput= document.querySelector("#txt-input");
+var txtOutput=document.querySelector("#output-div");
+var serverUrl="https://api.funtranslations.com/translate/minion.json?text=";
+function getTranslatorUrl(text){
+    return serverUrl+text
+}
+function errorHandler(error){
+    console.log("error occured: ",error);
+    alert("Something is wrong. Try again after sometime.")
+}
+
+
+function clickEventHandler(){
+ var inputText = txtInput.value;
+ var url= getTranslatorUrl(inputText);
+ 
+fetch(url)
+.then(response=> response.json())
+.then(json=>
+    txtOutput.innerHTML=json.contents.translated)
+
+ .catch(errorHandler);
+ 
+
+   
+}
+btnTranslate.addEventListener("click",clickEventHandler);
